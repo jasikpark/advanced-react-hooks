8c77227a02b9e8cfe0c777d213b15cab9565ef16
jasikpark <calebjasik@jasik.xyz>
2022-03-25 22:58:48 +0000

    Learn about useReducer and useCallback

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,24 @@
 
 import * as React from 'react'
 
+function countReducer(oldState, {type, step} = {}) {
+  switch (type) {
+    case 'INCREMENT':
+      return {count: oldState.count + step}
+    default:
+      return oldState
+  }
+}
+
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
